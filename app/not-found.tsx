eadffe01e1aab5aae90c0e7d05df0c87cd4d0a67
Anton Smatanik <smatanik.anton@gmail.com>
2024-10-10 11:01:39 +0200

    Add not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const NotFound = () => {
+  return (
+    <main className="space-y-5">
+      <h1 className="text-3xl">Not found</h1>
+      <p>Unfortunately, we could not find the requested page or resource.</p>
+    </main>
+  );
+};
+
+export default NotFound;
